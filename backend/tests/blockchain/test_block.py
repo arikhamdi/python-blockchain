86962f81f0915a5d30fa90a460bf1757e372d521
Arik <arik.hamdi@gmail.com>
2021-02-09 13:03:56 +0100

    hex to binary hash converter

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,5 +1,6 @@
 from backend.blockchain.block import Block, GENESIS_DATA
 from backend.config import MINE_RATE, SECONDS
+from backend.util.hex_to_binary import hex_to_binary
 
 import time
 
@@ -12,7 +13,8 @@ def test_mine_block():
     assert isinstance(block, Block)
     assert block.data == data
     assert block.last_hash == last_block.hash
-    assert block.hash[0:block.difficulty] == '0' * block.difficulty
+    assert hex_to_binary(block.hash)[
+        0:block.difficulty] == '0' * block.difficulty
 
 
 def test_genesis():
