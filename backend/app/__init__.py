380537823e8c07e2445e7fd6227723a196ed50f9
Arik <arik.hamdi@gmail.com>
2021-02-11 23:00:09 +0100

    transaction pool & update transaction

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from backend.blockchain.blockchain import Blockchain
 from backend.wallet.wallet import Wallet
 from backend.wallet.transaction import Transaction
+from backend.wallet.transaction_pool import TransactionPool
 from backend.pubsub import PubSub
 
 import requests
@@ -12,7 +13,8 @@ import os
 app = Flask(__name__)
 blockchain = Blockchain()
 wallet = Wallet()
-pubsub = PubSub(blockchain)
+transaction_pool = TransactionPool()
+pubsub = PubSub(blockchain, transaction_pool)
 
 
 @app.route('/')
@@ -40,11 +42,22 @@ def route_blockchain_mine():
 @app.route('/wallet/transact', methods=['POST'])
 def route_wallet_transact():
     transaction_data = request.get_json()
-    transaction = Transaction(
-        wallet,
-        transaction_data['recipient'],
-        transaction_data['amount']
-    )
+    transaction = transaction_pool.existing_transaction(wallet.address)
+
+    if transaction:
+        transaction.update(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
+    else:
+        transaction = Transaction(
+            wallet,
+            transaction_data['recipient'],
+            transaction_data['amount']
+        )
+
+    pubsub.broadcast_transaction(transaction)
 
     print(f"transaction.to_json(): {transaction.to_json()}")
     return jsonify(transaction.to_json())
--- backend/pubsub.py
+++ backend/pubsub.py
@@ -1,3 +1,4 @@
+from backend.wallet.transaction import Transaction
 from backend.blockchain.block import Block
 import time
 from pubnub.pubnub import PubNub
@@ -12,13 +13,15 @@ pnconfig.publish_key = 'pub-c-a3ad5aae-7455-4cf0-89e6-318ac9c6c262'
 
 CHANNELS = {
     'TEST': 'TEST',
-    'BLOCK': 'BLOCK'
+    'BLOCK': 'BLOCK',
+    'TRANSACTION': 'TRANSACTION'
 }
 
 
 class Listener(SubscribeCallback):
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
 
     def message(self, pubnub, message_object):
         print(
@@ -34,6 +37,10 @@ class Listener(SubscribeCallback):
                 print(f'\n -- Successfully replace the local chain')
             except Exception as e:
                 print(f'\n -- Did not replace chain: {e}')
+        elif message_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(message_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print(f"\n -- Set the new transaction in transaction pool")
 
 
 class PubSub():
@@ -42,10 +49,10 @@ class PubSub():
     Provides communication between the nodes of the blockchain network.
     """
 
-    def __init__(self, blockchain) -> None:
+    def __init__(self, blockchain, transaction_pool) -> None:
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener(blockchain))
+        self.pubnub.add_listener(Listener(blockchain, transaction_pool))
 
     def publish(self, channel, message):
         """
@@ -58,6 +65,12 @@ class PubSub():
         Broadcast a block object to all nodes.
         """
         self.publish(CHANNELS['BLOCK'], block.to_json())
+
+    def broadcast_transaction(self, transaction):
+        """
+        Broadcast a transaction to all nodes
+        """
+        self.publish(CHANNELS['TRANSACTION'], transaction.to_json())
 
 
 def main():
